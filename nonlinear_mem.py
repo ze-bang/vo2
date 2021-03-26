588dc2b678c1f85082005ee8f33ec26b1dc7798f
ze-bang <46905987+ze-bang@users.noreply.github.com>
2021-03-26 10:30:39 -0400

    Update nonlinear_mem.py

--- nonlinear_mem.py
+++ nonlinear_mem.py
@@ -6,23 +6,31 @@ kb = 1.38064852 * 10**(-23)
 q = 1.60217662 * 10**(-19)
 T_amp = 300
 T_c = 340
-d = 
-A = 
+d = 3*10^-6
+A = 2*10^(-12)
 c_th = 1050*10^(-12)
 e = q
-mu =  #electron mobility
-N_c = #effective density of state in conduction band
-N_d = #density of state of donor
-N_t = #density of state of traps
-E_d = #donor energy
-E_t = #trap energy
-epsilon_0 =  8.85418782 * 10*(-12)#permisivity of free space
-epsilon_i =  #high frequency dielectric constant
+mu =  0.15*10^(-2)#electron mobility
+E_d = 0#donor energy
+E_t = 0#trap energy
+#N_c = N_0#effective density of state in conduction band
+#N_d = N_c#density of state of donor
+#N_t = N_c#density of state of traps
+
+epsilon_0 =  8.85418782 * 10*(-12) #permisivity of free space
+epsilon_i =  50#high frequency dielectric constant
 T = T_amp
-t_L = #time evolve
-N = #steps
+t_L = 3#time evolve
+N = 1000000#steps
 dt = t_L/N#time step
 t = 0
+
+def N_0(temp):
+	if temp >= T_c:
+		return 10^23
+	else:
+		return 50^19
+
 
 def r_th(temp):
 	if temp >= T_c:
@@ -30,7 +38,7 @@ def r_th(temp):
 	else:
 		return 18000
 def sigma_0(temp):
-	return e*mu*N_c*(N_d/N_t)^2*exp(-(E_d+E_t)/(2*kb*temp))
+	return e*mu*N_0(temp)*(N_0(temp)/N_0(temp))^2*exp(-(E_d+E_t)/(2*kb*temp))
 
 def omega(temp):
 	return sqrt(q^3/(math.pi*epsilon_i*epsilon_0))
@@ -38,16 +46,17 @@ def omega(temp):
 def dT_dt(i_m, v_m, T):
 	return (i_m*v_m)/c_th - (T-Tamp)/(c_th*r_th(T))
 
-def i_m(v_m, T)
+def i_m(v_m, T):
 	return (sigma_0(T)*exp(-0.301/(2*kb*T))*A*(kb*T^2/omega(T))*(1+(omega(T)*sqrt(v_m/d)/(kb*T)-1)*exp((omega(T)*sqrt(v_m/d)/(kb*T))+1/(2*d))))*v_m
 
-def evolve(v_m, T, dt, t, counter):
+def evolve(v_m, T, dt, t, counter,N):
 	T = T + dT_dt(i_m(v_m,T),v_m,T)*dt
 	t = t+dt
-	evolve_v_m(v_m, t)
+	evolve_v_m(v_m, t,dt, N)
 	counter = counter + 1
 
-def evolve_v_m(v_m, t):
+def evolve_v_m(v_m, t, dt,N):
+        v_m = v_m+5/N*dt
 	#to control v_m
 
 i = np.zeros((1,N))
@@ -56,4 +65,4 @@ counter = 0
 while(t < t_L):
 	i[counter] = i_m(v_m,T)
 	v[counter] = v_m
-	evolve(v_m, T, dt, t, counter)
+	evolve(v_m, T, dt, t, counter,N)
